61d1ff298ddfeef3c66006f5de9d2bed112d2604
stephenhutchings <stephen@creativelicence.com.au>
2014-04-11 15:40:19 +1000

    re-compile source

--- Artboards2iOS-Exporter.jsx
+++ Artboards2iOS-Exporter.jsx
@@ -1,35 +1,33 @@
 // Stephen Hutchings (https://github.com/stephenhutchings)
 // MIT License Copyright (c) 2014
 
-var Exporter, sizes,
-  __hasProp = {}.hasOwnProperty;
+var Exporter, sizes;
 
-sizes = {
-  standard: {
+sizes = [
+  {
     suffix: "",
     scale: 100
-  },
-  retina: {
+  }, {
     suffix: "@2x",
     scale: 200
   }
-};
+];
 
 Exporter = {
   parentDir: Folder.selectDialog(),
   activeDoc: app.activeDocument,
   initialize: function() {
-    var size;
-    if (this.doc && this.parentDir) {
-      for (size in sizes) {
-        if (!__hasProp.call(sizes, size)) continue;
+    var size, _i, _len;
+    if (this.activeDoc && this.parentDir) {
+      for (_i = 0, _len = sizes.length; _i < _len; _i++) {
+        size = sizes[_i];
         this.exportArtboardsToFiles(size.scale, size.suffix);
       }
     }
   },
   exportArtboardsToFiles: function(scale, suffix) {
     var artboard, exportOptions, file, fileName, i, name, _i, _len, _ref;
-    _ref = this.doc.artboards;
+    _ref = this.activeDoc.artboards;
     for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
       artboard = _ref[i];
       name = artboard.name;
@@ -40,7 +38,7 @@ Exporter = {
         scale *= 0.5;
         name = name.slice(0, -3);
       }
-      this.doc.artboards.setActiveArtboardIndex(i);
+      this.activeDoc.artboards.setActiveArtboardIndex(i);
       fileName = this.createFoldersFromName(name);
       file = new File(fileName + suffix + ".png");
       exportOptions = new ExportOptionsPNG24();
@@ -49,7 +47,7 @@ Exporter = {
       exportOptions.antiAliasing = true;
       exportOptions.verticalScale = scale;
       exportOptions.horizontalScale = scale;
-      this.doc.exportFile(file, ExportType.PNG24, exportOptions);
+      this.activeDoc.exportFile(file, ExportType.PNG24, exportOptions);
     }
   },
   createFoldersFromName: function(name) {
@@ -59,7 +57,7 @@ Exporter = {
     _ref = dirs.slice(0, -1);
     for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
       item = _ref[i];
-      dir = new Folder(this.folder.fsName + "/" + dirs.slice(0, i + 1).join("/"));
+      dir = new Folder(this.parentDir.fsName + "/" + dirs.slice(0, i + 1).join("/"));
       if (!dir.exists) {
         dir.create();
       }
